37161f615e92bf5911dfeb9bf5b45edf0142dc66
Suzanne Millstein <smillst@cs.washington.edu>
2017-10-16 10:06:53 -0700

    Split packages.

--- .travis-build.sh
+++ .travis-build.sh
@@ -16,25 +16,33 @@ export CHECKERFRAMEWORK=$ROOT/checker-framework
 (cd $ROOT && hg clone https://bitbucket.org/typetools/annotated-jdk8u-jdk)
 
 ## Jdk
-
+## Compile all the packages for the following checkers:
 if [[ "$1" == "formatter" ]]; then
   PROCESSORS=formatter ./build8.sh
 elif [[ "$1" == "interning" ]]; then
   PROCESSORS=interning ./build8.sh
 elif [[ "$1" == "lock" ]]; then
   PROCESSORS=lock ./build8.sh
-elif [[ "$1" == "nullness-fbc" ]]; then
-  PROCESSORS=nullness ./build8.sh
-elif [[ "$1" == "nullness-raw" ]]; then
-  PROCESSORS=org.checkerframework.checker.nullness.NullnessRawnessChecker ./build8.sh
 elif [[ "$1" == "regex" ]]; then
   PROCESSORS=regex ./build8.sh
 elif [[ "$1" == "signature" ]]; then
   PROCESSORS=signature ./build8.sh
-elif [[ "$1" == "index" ]]; then
-  PROCESSORS=index ./build8.sh
 elif [[ "$1" == "value" ]]; then
   PROCESSORS=org.checkerframework.common.value.ValueChecker ./build8.sh
+
+## Spilt the jdk into two jobs.  The packages are grouped so that ~50% of the lines of code are in each job.
+elif [[ "$1" == "index-sun-javax" ]]; then
+  PROCESSORS=index PACKAGES="sun javax"./build8.sh
+elif [[ "$1" == "nullness-fbc-sun-javax" ]]; then
+  PROCESSORS=nullness PACKAGES="sun javax" ./build8.sh
+elif [[ "$1" == "nullness-raw-sun-javax" ]]; then
+  PROCESSORS=org.checkerframework.checker.nullness.NullnessRawnessChecker PACKAGES="sun javax" ./build8.sh
+elif [[ "$1" == "index-java-com-jdk-org" ]]; then
+  PROCESSORS=index PACKAGES="java com jdk org" ./build8.sh
+elif [[ "$1" == "nullness-fbc-java-com-jdk-org" ]]; then
+  PROCESSORS=nullness PACKAGES="java com jdk org" ./build8.sh
+elif [[ "$1" == "nullness-raw-java-com-jdk-org" ]]; then
+  PROCESSORS=org.checkerframework.checker.nullness.NullnessRawnessChecker PACKAGES="java com jdk org" ./build8.sh
 else
   echo "Bad argument '$1' to travis-build.sh"
   false
